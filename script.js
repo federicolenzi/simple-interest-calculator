fd76f9ee4af98562f6baf41892990dfc655d9271
federicolenzi <flenzi@fbk.eu>
2022-06-24 12:44:47 +0200

    Final release.

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     
+    // Check if the value entered in 'principal' textbox is zero or negative
+    if(principal == '' || principal <= 0 ) {
+        alert('Enter a positive amount for Principal!');
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = parseInt(document.getElementById("years").value);
+
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + years;
+ 
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\> \
+                                                 at an interest rate of <mark>"+rate+"%</mark>.\<br\> \
+                                                 You will receive an amount of <mark>"+interest+"</mark>,\<br\> \
+                                                 in the year <mark>"+year+"</mark>.\<br\>"
 }
-        
+  
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+'%';
+}
